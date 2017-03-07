ebcecb6c79a7375956137c5217a9a0427b0362f2
mindziukasss <mindziukasss@gmail.com>
2017-03-07 12:59:41 +0200

    Added post with jQuery and Ajax

--- wp-content/plugins/contact_widget/contact-widget.php
+++ wp-content/plugins/contact_widget/contact-widget.php
@@ -12,7 +12,7 @@ function add_scripts(){
   wp_enqueue_script('contact-scripts', plugins_url().'/contact-widget/js/script.js', array('jquery'),'1.0.0', false);
 }
 
-add_action('wp_enqueue_scripts', 'add_scripts');
+add_action('wp_enqueue_scripts','add_scripts');
 
 
 include('class.contact-widget.php');
@@ -22,4 +22,4 @@ function register_contact_widget(){
   register_widget('Contact_Widget');
 }
 
-add_action('widgets_init', 'register_contact_widget');
+add_action('widgets_init','register_contact_widget');
---
+++ wp-content/plugins/contact_widget/js/script.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function(){
+  var form = jQuery('#ajax-contact');
+  var formMessages = jQuery('#form-messages');
+
+  jQuery(form).submit(function(event) {
+    event.preventDefault();
+    console.log('Contact Form Submitted');
+    var formData = jQuery(form).serialize();
+
+    jQuery.ajax({
+      type: 'POST',
+      url: jQuery(form).attr('action'),
+      data: formData
+    }).done(function(response){
+        jQuery(formMessages).removeClass('error');
+        jQuery(formMessages).addClass('success');
+
+        jQuery(formMessages).text(response);
+
+        jQuery('#name').value('');
+        jQuery('#email').value('');
+        jQuery('#message').value('');
+
+
+    }).fail(function(data){
+        jQuery(formMessages).removeClass('success');
+        jQuery(formMessages).addClass('error');
+
+        if(data.responseText !== ''){
+          jQuery(formMessages).text(data.responseText);
+        }else {
+          jQuery(formMessages).text('An error occured');
+        }
+    });
+  });
+});
